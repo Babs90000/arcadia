e9c15afc208738f94fcbfb47a44c331ac06c20c3
Babs90000 <156372912+Babs90000@users.noreply.github.com>
2024-09-26 12:49:28 +0200

    Update create_admin.php

--- creation_role_admin/create_admin.php
+++ creation_role_admin/create_admin.php
@@ -1,21 +1,16 @@
 <?php
-// Connexion à la base de données
-try {
-    $base_de_donnees = new PDO('mysql:host=localhost;dbname=arcadia', 'root', '');
-    $base_de_donnees->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die('Erreur de connexion : ' . $e->getMessage());
-}
 
-// Vérifier si le formulaire a été soumis
+require_once '../env.php';
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Récupérer les données du formulaire
+  
     $username = $_POST['username'];
     $password = $_POST['password'];
     $nom = $_POST['nom'];
     $prenom = $_POST['prenom'];
 
-    // Hacher le mot de passe
+
     $password_hache = password_hash($password, PASSWORD_BCRYPT);
 
     // Insérer l'utilisateur administrateur avec le mot de passe haché
