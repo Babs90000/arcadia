90550a8781417414e45a0badcdbf84ae7675a4aa
Babs90000 <156372912+Babs90000@users.noreply.github.com>
2024-09-26 19:23:49 +0200

    Update espace_admin.php

--- pages/espace_admin.php
+++ pages/espace_admin.php
@@ -4,7 +4,7 @@ require_once '../template/header.php';
 
 
 if (!isset($_SESSION['role']) || $_SESSION['role'] !== 1) {
-
+ 
     header('Location: connexion_utilisateur.php');
     exit();
 }
@@ -27,25 +27,23 @@ switch ($role_utilisateur) {
         $role_label = 'Utilisateur';
         break;
 }
-$base_url = 'http://' . $_SERVER['arcadia-project-155bc9d56c41.herokuapp.com/'] . '/arcadia/';
 ?>
 <link rel=stylesheet href="./style_espace_utilisateur.css">
 <main>
-    <div class="container">
-        <h1>Bienvenue dans l'Espace Admin</h1>
-        <p>Bonjour <?php echo $prenom; ?>, vous êtes connecté !</p>
-        <a href="<?php echo $base_url; ?>fonctionnalités/gestion_service.php">Gestion des services</a><br>
-        <a href="<?php echo $base_url; ?>fonctionnalités/inscription_utilisateur.php">Gestion des utilisateurs</a><br>
-        <a href="<?php echo $base_url; ?>fonctionnalités/gestion_animaux.php">Gestion des animaux</a><br>
-        <a href="<?php echo $base_url; ?>pages/gestion_habitat.php">Gestion des habitats</a><br>
-        <a href="<?php echo $base_url; ?>fonctionnalités/modification_horaire.php">Modification des horaires</a><br>
-        <a href="<?php echo $base_url; ?>pages/rapports_veterinaires.php">Rapports vétérinaires</a><br>
-        <a href="<?php echo $base_url; ?>fonctionnalités/valider_avis.php">Valider un avis</a><br>
+        <div class="container">
+            <h1>Bienvenue dans l'Espace Admin</h1>
+            <p>Bonjour <?php echo $prenom; ?>, vous êtes connecté !</p>
+            <a href="../fonctionnalités/gestion_service.php">Gestion des services</a><br>
+            <a href="../fonctionnalités/inscription_utilisateur.php">Gestion des utilisateurs</a><br>
+            <a href="../fonctionnalités/gestion_animaux.php">Gestion des animaux</a><br>
+            <a href="../pages/gestion_habitat.php">Gestion des habitats</a><br>
+            <a href="../fonctionnalités/modification_horaire.php">Modification des horaires</a><br>
+            <a href="../pages/rapports_veterinaires.php">Rapports vétérinaires</a><br>
+            <a href="../fonctionnalités/valider_avis.php">Valider un avis </a><br>
+        
+        </div>
+    </main>
 
-    </div>
-</main>
-
-
+   
 </body>
-
 </html>
