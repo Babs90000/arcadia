0b3d60044a9080153916ac1376b0a25a349204f0
Babs90000 <156372912+Babs90000@users.noreply.github.com>
2024-09-28 01:59:57 +0200

    Update header.php

--- template/header.php
+++ template/header.php
@@ -61,6 +61,11 @@ foreach ($horaires as $horaire) {
   </head>
 
   <body>
+     <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    ?>
     <header>
       <nav>
         <img
