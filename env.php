abd768b51104e7148d3ba0ca4e29c7b367f09161
Babs90000 <156372912+Babs90000@users.noreply.github.com>
2024-09-28 23:26:12 +0200

    Update env.php

--- env.php
+++ env.php
@@ -1,14 +1,13 @@
 <?php
-
-if (getenv('JAWSDB_URL') !== false) {
-    $url = getenv('JAWSDB_URL');
-    $dbparts = parse_url($url);
-  
+// Vérifiez si les variables de connexion sont définies via une URL de base de données
+if (isset($_ENV['DATABASE_URL'])) {
+    $dbparts = parse_url($_ENV['DATABASE_URL']);
     $hostname = $dbparts['host'];
     $username = $dbparts['user'];
     $password = $dbparts['pass'];
     $database = ltrim($dbparts['path'], '/');
 } else {
+    // Sinon, utilisez les valeurs par défaut
     $hostname = 'localhost';
     $username = 'root';
     $password = '';
@@ -16,10 +15,12 @@ if (getenv('JAWSDB_URL') !== false) {
 }
 
 try {
-    
+    // Initialisez la connexion PDO
     $bdd = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
+    // Affichez un message d'erreur en cas d'échec de la connexion
     echo 'Erreur de connexion : ' . $e->getMessage();
     exit();
 }
+?>
